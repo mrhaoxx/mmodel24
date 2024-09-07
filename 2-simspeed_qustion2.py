d5066e89284291d0d77d4cbea7fa0a01501d7a4a
haoxingxing <mr.haoxx@gmail.com>
2024-09-07 23:26:47 +0800

    ckpt

--- 2-simspeed_qustion2.py
+++ 2-simspeed_qustion2.py
@@ -2,21 +2,20 @@ from utils import get_distance
 
 from core import loong, get_speed
 
-time = 0
+time = 412.4568550760522
 
-l = loong(pitch=55,  r_turning_space=0)
+l = loong(pitch=55, r_turning_space=0)
 
 def print_speed(speed):
     for s in speed:
         print(f"{s/100:.6f}", end=",")
     print()
 
-while time <= 300:
-    speed = get_speed(l, time, 100)
-    print_speed(speed)
-    time += 1
-    
-    
+speed = get_speed(l, time, 100)
+
+print_speed(speed)
+
+
 
 
 
